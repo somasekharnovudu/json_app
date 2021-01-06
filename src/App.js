ae9a00cc6b4bfe527eb0e2751b8f48f40aaacb04
soma sekhar novdu <somasekharnovdu@somas-MacBook-Pro.local>
2021-01-06 22:20:56 +0530

    sample useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const getData = async (personId) => {
+  try {
+    const resp = await axios.get('https://jsonplaceholder.typicode.com/users/' + personId)
+    return resp.data
+  } catch (err) {
+    console.log(err)
+  }
+}
+
 function App() {
+  const [personObj, setPersonObj] = useState({});
+  useEffect(() => {
+    async function getPersonDetails() {
+      const personData = await getData(5);
+      setPersonObj(personData)
+    }
+    getPersonDetails()
+  }, [])
+  const headers = {
+    id: 'Id',
+    name: 'Name',
+    username: 'User Name'
+  }
+  const headerKeys = Object.keys(headers);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <thead>
+          <tr>
+            {headerKeys.map((labelKey) => <th key={labelKey}>{headers[labelKey]}</th>)}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {headerKeys.map((labelKey) => <td key={personObj[labelKey]}>{personObj[labelKey]}</td>)}
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
