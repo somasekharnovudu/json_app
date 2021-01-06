812a007060f0923e48ee3dfa28b06d66c856ad9b
soma sekhar novdu <somasekharnovdu@somas-MacBook-Pro.local>
2021-01-06 22:53:11 +0530

    todo app implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-const getData = async (personId) => {
+const getData = async (toDoId) => {
   try {
-    const resp = await axios.get('https://jsonplaceholder.typicode.com/users/' + personId)
+    const resp = await axios.get('https://jsonplaceholder.typicode.com/todos/' + toDoId)
     return resp.data
   } catch (err) {
     console.log(err)
@@ -12,34 +12,47 @@ const getData = async (personId) => {
 }
 
 function App() {
-  const [personObj, setPersonObj] = useState({});
-  useEffect(() => {
-    async function getPersonDetails() {
-      const personData = await getData(5);
-      setPersonObj(personData)
-    }
-    getPersonDetails()
-  }, [])
+  const [todoId, setToDoId] = useState('');
+  const [toDoitem, setToDoitem] = useState({})
+  const getToDoItem = async () => {
+    const resp = await getData(todoId)
+    setToDoitem(resp)
+  }
   const headers = {
     id: 'Id',
-    name: 'Name',
-    username: 'User Name'
+    title: 'Name',
+    completed: 'Completed'
   }
   const headerKeys = Object.keys(headers);
   return (
     <div className="App">
-      <table>
-        <thead>
-          <tr>
-            {headerKeys.map((labelKey) => <th key={labelKey}>{headers[labelKey]}</th>)}
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            {headerKeys.map((labelKey) => <td key={personObj[labelKey]}>{personObj[labelKey]}</td>)}
-          </tr>
-        </tbody>
-      </table>
+      <div className="formContainer">
+        <div className="inputContainer">
+          <label htmlFor="todoId">Id: </label>
+          <input type="text" id='todoId' value={todoId} onChange={(e) => setToDoId(e.target.value)} />
+        </div>
+        <button onClick={getToDoItem}>Submit</button>
+      </div>
+      {
+        toDoitem.id && <table>
+          <thead>
+            <tr>
+              {headerKeys.map((labelKey) => <th key={labelKey}>{headers[labelKey]}</th>)}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              {headerKeys.map((labelKey) => {
+                let dataItem = <td key={toDoitem[labelKey]}>{toDoitem[labelKey]}</td>
+                if (labelKey === 'completed') {
+                  dataItem = <td key={toDoitem[labelKey]}>{toDoitem[labelKey] === true ? 'Completed' : 'Not Completed'}</td>
+                }
+                return dataItem
+              })}
+            </tr>
+          </tbody>
+        </table>
+      }
     </div>
   );
 }
